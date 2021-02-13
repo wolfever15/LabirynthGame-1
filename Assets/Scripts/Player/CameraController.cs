e3158f8261db6ad1de178153a258fa08c33dbaf6
wolfever15 <adamgoral@onet.eu>
2021-02-13 13:29:27 +0100

    Camera
    Camera Script

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float mouseSensivity = 100f;
+    public Transform playerBody = null;
+    private float xRotation = 0f;
+
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void Update()
+    {
+        playerBody = transform.parent;
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;
+        
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 80f);
+
+        transform.localRotation = Quaternion.Euler(xRotation,0f,0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
