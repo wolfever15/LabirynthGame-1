7625c128211d93e05da2a9c38621b1be90e19d9d
wolfever15 <adamgoral@onet.eu>
2021-02-20 13:09:38 +0100

    dodany Game Manager
    obsluguje koniec i wynik gry

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gameManager { get; private set; }
+    public int timeToEnd = 240;
+    public bool gamePause = false;
+
+    private bool endGame = false;
+    private bool win = false;
+
+    public void EndGame()
+    {
+        CancelInvoke("Stopper");
+        if (win) Debug.Log("You win!! Reload?");
+        else Debug.Log("You lose!! Reload?");
+    }
+
+    private void Awake()
+    {
+        if(gameManager==null) gameManager = this;
+    }
+    public void PauseGame()
+    {
+        Debug.Log("Game paused");
+        gamePause = true;
+        Time.timeScale = 0f;
+    }
+    public void ResumeGame()
+    {
+        Debug.Log("Game resume");
+        gamePause = false;
+        Time.timeScale = 1f;
+    }
+    public void Stopper()
+    {
+        timeToEnd--;
+        Debug.Log("Time: " + timeToEnd + "s");
+        if (timeToEnd <= 0f)
+        {
+            timeToEnd = 0;
+           
+        }else win = true;
+
+        if (endGame) EndGame();
+
+    }
+    
+    void Start()
+    {
+        InvokeRepeating("Stopper", 2, 1);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gamePause==true)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+           
+
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 12f;
+    [SerializeField] private float gravity = -10f;
+    private Vector3 velocity;
+
+    private CharacterController characterController = null;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        characterController.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+        
+    }
+}
